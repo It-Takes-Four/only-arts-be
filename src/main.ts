da6f073b3dc2767bcd3ee6678eed9f4f5ecd2600
Marvin Luckianto <marvin.luckianto002@binus.ac.id>
2025-07-23 14:35:22 +0700

    feat: integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('OnlyArts API') 
+    .setDescription('The OnlyArts API Documentation')
+    .setVersion('1.0')
+    .addTag('default')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); 
 
   await app.listen(3000);
 }
